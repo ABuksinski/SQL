5a029bf7407d57f5c07d1baf13fc096e3fdb5f41
ABuksinski <ABuksinski@qa.com>
2021-01-21 12:55:03 +0000

    it shows error but it works so IDK

---
+++ flowchart1.java
@@ -0,0 +1,17 @@
+package exercise;
+
+public class flowchart {
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 10;
+		boolean notBool = true;
+		
+		if (notBool = true) {
+			int add = num1 + num2;
+			System.out.println(add);
+		} else {
+			int multiply = num1 * num2;
+			System.out.println(multiply);
+		}
+	}
+}
