173f0e8a79b6e9a81e17295aa4008626f14d7542
ABuksinski <ABuksinski@qa.com>
2021-01-21 10:35:21 +0000

    adding java files even tho they are wrong

---
+++ calculator.java
@@ -0,0 +1,17 @@
+package exercise;
+
+public class calculator {
+	{
+		int num1 = 1;
+		int num2 = 2;
+		int addition = num1 + num2;
+		System.out.println(addition);
+		int subtraction = num2 - num1;
+		System.out.println(subtraction);
+		int multiplication = num1 * num2;
+		System.out.println(multiplication);
+		int division = num1 / num2;
+		System.out.println(division);
+	}
+}
+
---
+++ results.java
@@ -0,0 +1,17 @@
+package exercise;
+
+public class Results {
+public static void method1()	{
+		int p = 100;
+		System.out.println(p);
+		int c = 100;
+		System.out.println(c);
+		int b = 100;
+		System.out.println(b);}
+public static void method2() {
+		int total = p + c + b;
+		int percentage = (total * 100) / 450;
+		System.out.println(percentage);
+	}
+}
+
