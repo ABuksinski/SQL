6db70860cab204184194a1c68d2b3a370c6dc04b
ABuksinski <ABuksinski@qa.com>
2021-01-21 14:55:40 +0000

    results updated

--- results.java
+++ results.java
@@ -1,17 +1,31 @@
 package exercise;
 
 public class Results {
-public static void method1()	{
-		int p = 100;
+
+	public static void main(String[] args) {
+
+		int p = 84;
 		System.out.println(p);
-		int c = 100;
+		int c = 150;
 		System.out.println(c);
-		int b = 100;
-		System.out.println(b);}
-public static void method2() {
-		int total = p + c + b;
-		int percentage = (total * 100) / 450;
-		System.out.println(percentage);
+		int b = 150;
+		System.out.println(b);
+		int t = p + b + c;
+		System.out.println(t);
+		int per = (t * 100) / 450;
+		System.out.println(per);
+		if (per < 60) {
+			System.out.println("fail overall");
+		} else if (((p * 100) / 150) < 60) {
+			System.out.println("fail physics");
+		} else if (((c * 100) / 150) < 60) {
+			System.out.println("fail chemistry");
+		} else if (((b * 100) / 150) < 60) {
+			System.out.println("fail biology");
+		} else {
+			System.out.println("Pass, Well Done");
+		}
 	}
+
 }
 
