637ba4dadc41addaee81457435f53ae95b5ad8f3
ABuksinski <ABuksinski@qa.com>
2021-01-21 12:06:50 +0000

    calculator exercise works!

--- calculator.java
+++ calculator.java
@@ -1,9 +1,10 @@
 package exercise;
 
 public class calculator {
-	{
-		int num1 = 1;
-		int num2 = 2;
+	public static void main(String[] args){
+		int num1 = 2;
+		int num2 = 1;
+		if (num1<num2) {
 		int addition = num1 + num2;
 		System.out.println(addition);
 		int subtraction = num2 - num1;
@@ -11,7 +12,9 @@ public class calculator {
 		int multiplication = num1 * num2;
 		System.out.println(multiplication);
 		int division = num1 / num2;
-		System.out.println(division);
+		System.out.println(division);}
+		else {
+			System.out.println("division cannot be performed");}
+		}
 	}
-}
 
